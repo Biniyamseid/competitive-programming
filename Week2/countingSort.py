99a22b5a5785ab42a8f3313f9ff7b612a3eceee9
Biniyam Seid <biniyamseid3@gmail.com>
2021-12-16 19:44:45 +0300

    countingsort

---
+++ Week2/countingSort.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def countingSort(arr):
+    # Write your code here
+    l = [0 for _ in range(100)]
+    for i in arr:
+        l[i] += 1
+    return l
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

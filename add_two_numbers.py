d98e2bce05c7dae7bdfd7774c6b1369b39576bb9
Your Name <you@example.com>
2022-05-10 11:23:14 +0300

    forth commit

---
+++ add_two_numbers.py
@@ -0,0 +1,20 @@
+class ListNode(object):
+    def __init__(self,x):
+        self.val = x
+        self.next = None
+class solution(object):
+    def addTwoNumbers(self,l1,l2):
+        prev = result = ListNode(None)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            prev.next = Listnode(carry % 10)
+            prev = prev.next
+            carry //= 10
+        return result.next
+        

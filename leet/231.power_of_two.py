91d875168c3353bac825eb8a241251a680029a3d
Biniyam Seid <Biniyamseid3@gmail.com>
2022-05-17 17:47:01 +0300

    solved

--- leet/231.power_of_two.py
+++ leet/231.power_of_two.py
@@ -9,3 +9,12 @@ def isPowerOfTwo(n: int) -> bool:
             count += 1
     return True if count == 1 else False
 print(isPowerOfTwo(4))
+#or
+def hammingWeight(n):
+    c = 0
+    while n:
+        n &= n-1
+        c += 1
+    return c
+#n &= n-1 gives 0000 for only the numbers which are the power of two
+

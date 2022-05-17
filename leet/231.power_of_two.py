6afd1fd5548ff7a1db654b9d7b94337c1fc0a3b8
Biniyam Seid <Biniyamseid3@gmail.com>
2022-05-17 17:07:53 +0300

    solved

--- leet/231.power_of_two.py
+++ leet/231.power_of_two.py
@@ -1,7 +1,10 @@
-def isPowerOfTwo(num):
-    k = bin(num)
+def isPowerOfTwo(n: int) -> bool:
+    k = bin(n)
     count = 0
+    Flag = True
     for i in k:
+        if i == "-":
+            return False
         if i == "1":
             count += 1
     return True if count == 1 else False

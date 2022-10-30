74878367e88c10e925ce58dc53256b3f60d2a2fb
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-10-30 22:43:35 +0300

    Time: 1870 ms (22.29%), Space: 22.2 MB (17.89%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,26 +1,25 @@
 class Solution:
-    def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])
-        if k >= m + n - 2: return m + n - 2  # if we can go by manhattan distance -> let's go
-        
-        DIR = [0, 1, 0, -1, 0]
-        q = deque([(0, 0, k)])  # pair of (r, c)
+    def shortestPath(self,grid,k):
+        m,n = len(grid),len(grid[0])
+        direct = [(0,1),(1,0),(0,-1),(-1,0)]
+        q = ([(0,0,k)])
         seen = set()
-        seen.add((0, 0, k))
+        seen.add((0,0,k))
         step = 0
         while q:
             for _ in range(len(q)):
-                r, c, k = q.popleft()
-                if r == m - 1 and c == n - 1: return step  # Reach to the bottom right cell
+                r,c,k = q.pop(0)
+                if r >= m-1 and c >= n-1:
+                    return step
                 for i in range(4):
-                    nr, nc = r + DIR[i], c + DIR[i + 1]
-                    if nr < 0 or nr == m or nc < 0 or nc == n: continue  # Skip out of bound cells!
-                    newK = k - grid[nr][nc]
-                    newState = (nr, nc, newK)
-                    if newK >= 0 and newState not in seen:
-                        seen.add(newState)
-                        q.append(newState)
-
+                    nr = direct[i][0]+r
+                    nc = direct[i][1]+c
+                    if nr < 0 or nr >= m or nc < 0 or nc>= n:
+                        continue
+                    newk = k -  grid[nr][nc]
+                    newstate = (nr,nc,newk)
+                    if newk >= 0 and newstate not in seen:
+                        seen.add(newstate)
+                        q.append(newstate)
             step += 1
-
         return -1

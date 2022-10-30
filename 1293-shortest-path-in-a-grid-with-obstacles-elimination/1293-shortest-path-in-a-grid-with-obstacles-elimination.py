fafb1750c67d21cdfb9c609e42dbdacc6b86f552
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-10-30 23:06:26 +0300

    Time: 2945 ms (5.94%), Space: 22.7 MB (7.14%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,25 +1,25 @@
 class Solution:
-    def shortestPath(self,grid,k):
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
         m,n = len(grid),len(grid[0])
-        direct = [(0,1),(1,0),(0,-1),(-1,0)]
-        q = ([(0,0,k)])
+        direction = [(0,1),(1,0),(-1,0),(0,-1)]
+        q = [(0,0,k)]
+        step = 0
         seen = set()
         seen.add((0,0,k))
-        step = 0
         while q:
             for _ in range(len(q)):
                 r,c,k = q.pop(0)
-                if r >= m-1 and c >= n-1:
+                print(r,c,k)
+                if r == m-1 and c == n-1:
                     return step
-                for i in range(4):
-                    nr = direct[i][0]+r
-                    nc = direct[i][1]+c
-                    if nr < 0 or nr >= m or nc < 0 or nc>= n:
+                for i in range(len(direction)):
+                    nr,nc = direction[i][0] + r, direction[i][1] + c
+                    if  nr < 0 or nr >= m or nc < 0 or nc >=n:
                         continue
-                    newk = k -  grid[nr][nc]
+                    newk = k - grid[nr][nc]
                     newstate = (nr,nc,newk)
                     if newk >= 0 and newstate not in seen:
-                        seen.add(newstate)
                         q.append(newstate)
+                        seen.add(newstate) 
             step += 1
         return -1

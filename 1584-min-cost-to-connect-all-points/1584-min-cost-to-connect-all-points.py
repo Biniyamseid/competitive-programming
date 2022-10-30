e2961559fc40d5e6a23b02016701494e9e857823
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-10-31 01:04:23 +0300

    Time: 2609 ms (79.32%), Space: 81.9 MB (66.51%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,50 +1,42 @@
 class DisjointSet:
     def __init__(self, n):
         self.parent = [i for i in range(n)]
-        self.rank = [1 for _ in range(n)]
+        self.count = n
+        #self.rank = [1 for _ in range(n)]
     
     # make a and b part of the same component
     # union by rank optimization
     def union(self, a, b):
         pa = self.find(a)
         pb = self.find(b)
-        if pa == pb: return
-        if self.rank[pa] > self.rank[pb]:
-            self.parent[pb] = pa
-            self.rank[pa] += self.rank[pb]
-        else:
-            self.parent[pa] = pb
-            self.rank[pb] += self.rank[pa]
+        if pa == pb: return False
+        self.parent[pb] = pa
+        self.count -= 1
+        return True
+        
     
     # find the representative of the 
     # path compression optimization
     def find(self, a):
-        if self.parent[a] == a:
-            return a
-        
-        self.parent[a] = self.find(self.parent[a])
-        return self.parent[a]
-    
+        while self.parent[a] != a:
+            a = self.parent[a]
+        return a
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        manhattan = lambda x,y:abs(x[0]-y[0])+abs(x[1]-y[1])
         
         n = len(points)
         edges = []
         for i in range(n):
             for j in range(i+1, n):
-                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                dist = manhattan(points[i],points[j])
                 edges.append((dist, i, j))
-        
-        # sort based on cost (i.e. distance)
-        edges.sort()
-        
-        # using Kruskal's algorithm to find the cost of Minimum Spanning Tree
-        res = 0
+        heapq.heapify(edges)
         ds = DisjointSet(n)
-        for cost, u, v in edges:
-            if ds.find(u) != ds.find(v):
-                ds.union(u, v)
+        res = 0       
+        while edges and ds.count>1:
+            cost,u,v = heapq.heappop(edges)
+            if ds.union(u,v):
                 res += cost
-        
         return res

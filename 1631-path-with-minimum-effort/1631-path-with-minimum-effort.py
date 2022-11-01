0d664e585b391c51fe4d67fb857e31434333fdf2
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-11-01 17:37:36 +0300

    Time: 1180 ms (81.49%), Space: 16.4 MB (40.51%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,23 +1,22 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        grid = heights
+        n,m = len(heights),len(heights[0])
+        target = (n-1,m-1)
         heap = [(0,(0,0))]
-        min_cost = defaultdict(lambda:float("inf"))
-        target = (len(heights)-1,len(heights[0])-1)
+        dist = {(0,0):0}
+        abs_dif = lambda x,y:abs()
         while heap:
-            cost,coordinate = heapq.heappop(heap)
-            n,m = coordinate
-            if coordinate == target:
+            cost,node = heapq.heappop(heap)
+            x,y = node
+            if node == target:
                 break
-            directions = [(0,1),(1,0),(0,-1),(-1,0)]
-            for p,q in directions:
-                newX,newY = p+n,q+m
-                if newX >= 0 and newX<=len(grid)-1 and newY>=0 and newY <= len(grid[0])-1:
-                    max_abs_difference_so_far_in_this_path = max(cost,abs(grid[n][m]-grid[newX][newY]))
-                    if (newX,newY) not in min_cost or min_cost[(newX,newY)] > max_abs_difference_so_far_in_this_path:
-                    #ከዚህ በፊት በሌላ መንገድ መጥቶ ነበረ አሁን ደግሞ በሌላ መንገድ ሲመትጣ ትንሹን እናስቀምጥለት
-                        min_cost[(newX,newY)] = min(max_abs_difference_so_far_in_this_path,min_cost[(newX,newY)])
-                        heapq.heappush(heap,(max_abs_difference_so_far_in_this_path,(newX,newY)))
-        #print(min_cost)
-        return min_cost[target] if min_cost[target] != float("inf") else 0
-            
+            for i,j in [(0,1),(0,-1),(-1,0),(1,0)]:
+                newx,newy = x+i,y+j
+                if newx >=0 and newx<n and newy<m and newy >=0:
+                    newedge = max(cost,abs(heights[newx][newy] - heights[x][y]))
+                    if (newx,newy) not in dist or newedge < dist[(newx,newy)]:
+                        dist[(newx,newy)] = newedge
+                        heapq.heappush(heap,(newedge,(newx,newy)))
+        #print(dist)
+        #print("hello")
+        return dist[(target)]

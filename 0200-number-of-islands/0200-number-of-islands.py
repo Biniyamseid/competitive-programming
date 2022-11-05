654237f97006109534dca1b3a9627bc377f64788
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-11-05 08:01:44 +0300

    Time: 1028 ms (16.73%), Space: 21.8 MB (27.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,10 +2,9 @@ from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def inrange(r,c):
-            if r>=0 and r < len(grid) and c>=0 and c<len(grid[0]):
+            if r >= 0 and r <len(grid) and c >= 0 and c< len(grid[0]):
                 return True
             return False
-        rows,cols = len(grid),len(grid[0])
         visited = set()
         
         res = 0
@@ -15,18 +14,19 @@ class Solution:
             visited.add((r,c))
             while q:
                 row,col = q.popleft()
-                directions = [(1,0),(-1,0),(0,1),(0,-1)]
-                for dr,dc in directions:
-                    newr,newc = row+dr,col+dc
-                    if inrange(newr,newc) and grid[newr][newc] == "1" and (newr,newc) not in visited:
-                        q.append((newr,newc))
-                        visited.add((newr,newc))
-        for r in range(rows):
-            for c in range(cols):
-                if (r,c) not in visited and  grid[r][c] == "1":
-                    bfs(r,c)
+                direction = [(0,1),(1,0),(0,-1),(-1,0)]
+                for dr,dc in direction:
+                    new_r,new_c = row+dr,col+dc
+                    if inrange(new_r,new_c) and grid[new_r][new_c] == "1" and (new_r,new_c) not in visited:
+                        q.append((new_r,new_c))
+                        visited.add((new_r,new_c))
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if (r,c) not in visited and grid[r][c] == "1":
                     res += 1
+                    bfs(r,c)
         return res
+                    
                         
                    
         

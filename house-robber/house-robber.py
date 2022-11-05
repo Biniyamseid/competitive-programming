0d0adaa827b6c92e6662020d73e6cdacfcc4e325
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-11-05 09:22:42 +0300

    Time: 43 ms, Memory: 13.9 MB - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -11,18 +11,10 @@ class Solution:
         """
         if len(nums) ==1:
             return nums[0]
-        memo = {0:nums[0],1:max(nums[0],nums[1])}
-        
-        def dfs(i):
-            if i in memo:
-                return memo[i]
-            if i>len(nums):
-                return 0
-            if i< 0:
-                return 0
-            memo[i] = max(dfs(i-1),nums[i] + dfs(i-2))
-            return memo[i]
-        return dfs(len(nums)-1)
+        nums[1] = max(nums[0],nums[1])
+        for i in range(2,len(nums)):
+            nums[i] = max(nums[i-1],nums[i-2]+nums[i])
+        return max(nums[-1],nums[-2])
             
             
         

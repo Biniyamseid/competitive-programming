5dc6d203df39dafed1cdd7c5b8ef06e2788079d9
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-11-08 06:44:36 +0300

    Time: 80 ms (21.98%), Space: 13.8 MB (61.93%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,19 +1,9 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        n = len(s)
-        s = list(s)
-        l = 0
-        r = 1
-        stack = []
-        for i in s:
-            flag = False
-            if stack and stack[-1].lower() == i.lower() and stack[-1] != i:
-                flag = True
-                stack.pop()
-            if not flag:
-                stack.append(i)
-        #print("hello world this is the national and tahe lateast")
-        return "".join(stack)
+        for i in range(len(s)-1):
+            if abs(ord(s[i]) - ord(s[i+1])) == 32:
+                return self.makeGood(s[:i]+s[i+2:])
+        return s
             
             
                 

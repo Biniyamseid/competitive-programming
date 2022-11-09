a718181de3c76c7d581a21985401d7202e9ff8bc
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-11-09 15:36:39 +0300

    Time: 137 ms (31.86%), Space: 14.6 MB (83.52%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,15 +1,14 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        result = []
-        def backtrack(i,permutation):
+        def backtrack(i,permutation,result):
             if i >= len(s):
                 result.append(permutation)
             else:
                 if s[i].isalpha():
-                    backtrack(i+1,permutation+s[i].upper())
-                    backtrack(i+1,permutation+s[i].lower())
+                    backtrack(i+1,permutation+s[i].upper(),result)
+                    backtrack(i+1,permutation+s[i].lower(),result)
                 else:
-                    backtrack(i+1,permutation+s[i])
-        backtrack(0,"")
-        return result
+                    backtrack(i+1,permutation+s[i],result)
+            return result
+        return backtrack(0,"",[])
         

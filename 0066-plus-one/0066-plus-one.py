37ce2c8f3909b217ef87510fe9b4eb6eba956ea3
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-11-11 19:03:49 +0300

    Time: 77 ms (5.52%), Space: 13.9 MB (11.88%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self,digits):
+        num = 0
+        for i in range(len(digits)):
+            num += digits[i] * pow(10, (len(digits)-1-i))
+        return [int(i) for i in str(num+1)]
+        
+        
+            
+        

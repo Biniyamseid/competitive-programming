7b59543a9af9f3e938c0eb99056ec30ec98faf22
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-11-13 07:21:25 +0300

    Time: 67 ms (22.13%), Space: 13.8 MB (66.22%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        if len(nums) ==1:
+            return nums[0]
+        dp=[nums[0],max(nums[0],nums[1])]
+        for i in range(2,len(nums)):
+            dp.append(0)
+            dp[i] = max(dp[i-1],nums[i]+dp[i-2])
+        return dp[-1]
+        """
+        last = 0
+        now = 0
+        for i in nums:
+            last,now = now,max(last+i,now)
+        return now
+            
+            
+        

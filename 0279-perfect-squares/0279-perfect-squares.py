0edb31956fd28212914758ae0b6f62b1c090bca9
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-12-02 00:34:06 +0300

    Time: 479 ms (72.27%), Space: 14.6 MB (16.11%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numSquares(self, n):
+        if n < 2:
+            return n
+        lst = []
+        i = 1
+        while i * i <= n:
+            lst.append( i * i )
+            i += 1
+        cnt = 0
+        toCheck = {n}
+        while toCheck:
+            cnt += 1
+            temp = set()
+            for x in toCheck:
+                for y in lst:
+                    if x == y:
+                        return cnt
+                    if x < y:
+                        break
+                    temp.add(x-y)
+            toCheck = temp
+
+        return cnt

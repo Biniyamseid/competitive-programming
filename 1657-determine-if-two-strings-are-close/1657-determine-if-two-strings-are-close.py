43b56b1ae385b75542f213b88751c4bc443e82f8
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-12-02 08:02:03 +0300

    Time: 223 ms (82.00%), Space: 16.5 MB (5.20%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -3,21 +3,15 @@ class Solution:
             from collections import Counter
             if len(word1) != len(word2):
                 return False
-            if set(word1) == set(word2):
+            if set(word1) == set(word2):#if the values of each strings are equal return True
                 if sorted(word1) == sorted(word2):
                     return True
-
                 count1 = Counter(word1)
                 count2 = Counter(word2)
                 lst1 = sorted(list(count1.values()))
-                print(lst1)
                 lst2 = sorted(list(count2.values()))
-                if lst1 == lst2:
+                if lst1 == lst2:#compare the count of each values
                     return True
                 return False
             return False
-        
-        
-        
-        
-        
+              

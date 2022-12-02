c071637ac1ec215a3ae863b88e6d2a58704acdcb
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2022-12-02 07:45:21 +0300

    Time: 220 ms (37.88%), Space: 14.1 MB (97.02%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,42 @@
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        merged = nums1+nums2
+        merged.sort()
+        if len(merged)%2:
+            return merged[len(merged)//2]
+        else:
+            return (merged[len(merged)//2-1]+merged[len(merged)//2])/2
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        m, n = len(nums1), len(nums2)
+        
+        left_size = (m + n + 1) // 2
+        start = 0
+        end = m
+        is_even = ((m + n) % 2) == 0
+        while start <= end:
+            a_part = (start + end) // 2
+            b_part = left_size - a_part
+            
+            aleftmax = float("-inf") if a_part == 0 else nums1[a_part - 1]
+            arightmin = float("inf") if a_part == m else nums1[a_part]
+            bleftmax = float("-inf") if b_part == 0 else nums2[b_part - 1]
+            brightmin = float("inf") if b_part == n else nums2[b_part]
+            
+            if aleftmax <= brightmin and bleftmax <= arightmin:
+                if not is_even:
+                    return max(aleftmax, bleftmax)
+                else:
+                    return (max(aleftmax, bleftmax) + min(arightmin, brightmin))/ 2
+            elif aleftmax > brightmin:
+                end = a_part - 1
+            elif bleftmax > arightmin:
+                start = a_part + 1
+            

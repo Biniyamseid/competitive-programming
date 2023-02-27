bd1696cd88d264f8a414acf4c0a5fce519e29933
Biniyamseid <88603119+Biniyamseid@users.noreply.github.com>
2023-02-27 07:19:31 +0300

    Time: 44 ms (18.04%), Space: 13.8 MB (57.92%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,14 +4,17 @@ class Solution:
         res = []
         for i in range(n):
             res.append([1]*(i+1))
-            
-        print(res)
-        for i in range(2,n):
+
+        def recur(i):
+            if i>=n:
+                return 
             for j in range(i):
                 if j == 0:
                     pass
                 else:
                     res[i][j] = res[i-1][j-1]+res[i-1][j]
+            recur(i+1)
+        recur(2)
         print(res)
         return res
     

588db06e846d0885aa919e4c371cab5c1a14c867
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-01-18 17:56:47 +0200

    Time: 26 ms (97.85%) | Memory: 17.5 MB (15.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,10 +2,10 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         @cache
         def dp(q):
-            if q<=1:
+            if q>=n:
                 return 1
-            step1= 0+dp(q-1)
-            step2 = 0+dp(q-2)
+            step1= 0+dp(q+1)
+            step2 = 0+dp(q+2)
             return step1+step2
-        return dp(n)            
+        return dp(1)            
         

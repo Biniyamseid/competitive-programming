379d2ea5c1be613833b7797309b27fefb5eb566d
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-01-29 19:51:45 +0200

    Time: 16 ms (100.00%) | Memory: 16.4 MB (92.68%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.py
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -2,19 +2,12 @@ class MyQueue:
 
     def __init__(self):
         self.q = []
-        
-
     def push(self, x: int) -> None:
         self.q.append(x)
-        
-
     def pop(self) -> int:
         return self.q.pop(0)
-        
-
     def peek(self) -> int:
         return self.q[0]
-
     def empty(self) -> bool:
         return len(self.q)==0
         

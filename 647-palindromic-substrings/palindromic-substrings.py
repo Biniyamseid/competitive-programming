d40a4d4571458c870b316ede1ee45df1227aa0af
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-02-10 22:53:42 +0300

    Time: 907 ms (5.21%) | Memory: 172.6 MB (6.74%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,17 +1,15 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        
-        @lru_cache(maxsize=None)
-        def dfs(start, end):
+        @cache
+        def dfs(start,end):
             nonlocal result
             if s[start:end+1] == s[start:end+1][::-1]:
                 result +=1
-            
-            # loop with one less character
-            if start < end:
-                dfs(start + 1, end)
-                dfs(start, end-1)
-            
-        result = 0
-        dfs(0, len(s) - 1)
+            if start<end:
+                dfs(start+1,end)
+                dfs(start,end-1)
+        result =0
+        dfs(0,len(s)-1)
         return result
+            
+        

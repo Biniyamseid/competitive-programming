02379edb87db124c7471d30c08d8cf0af6995aa7
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-02-28 12:45:17 +0300

    Time: 50 ms (15.28%) | Memory: 18.6 MB (39.85%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        maxdepth = 0
+        ans = root.val
+        def dfs(node,depth):
+            nonlocal maxdepth
+            nonlocal ans
+            if not node:
+                return
+            if node.right and depth>maxdepth:
+                ans = node.right.val
+            if node.left and depth>maxdepth:
+                ans = node.left.val
+            if node and depth>maxdepth:
+                ans = node.val
+            print(depth,maxdepth,node.val,ans)
+            maxdepth = max(depth,maxdepth)
+            dfs(node.left,depth+1)
+            dfs(node.right,depth+1)
+            
+        dfs(root,1)
+        return ans
+            
+        

fdc0c037c03970f326299b9367e88bf06a8bf479
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-04-02 05:49:32 +0300

    Time: 38 ms (82.33%) | Memory: 16.8 MB (32.93%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,44 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        ins = set()
+        out = set()
+        for a,b in zip(s,t):
+            ins.add(a)
+            out.add(b)
+        if len(ins)!= len(out):
+            return False
+       
+        checker = defaultdict(lambda :"0")
+        
+        notsame = set()
+        same = set()
+
+        for a,b in zip(s,t):
+            if a in same or b in same:
+                if a!=b:
+                    return False
+            
+            if checker[a]!="0":
+                if checker[a]==b:
+                    pass
+                else:
+                    print(checker,"first")
+                    return False
+       
+
+            elif checker[a]=="0":
+                checker[a] = b
+            
+            if a==b:
+                same.add(a)
+                if a in notsame or b in notsame:
+                    return False
+                
+            else:
+                notsame.add(a)
+                notsame.add(b)
+            
+
+        print(checker)
+        return True
+        

611a2e36ea6b85e3a177599af6b32c29a6da0d03
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-04-05 22:24:21 +0300

    Time: 46 ms (9.86%) | Memory: 16.5 MB (91.04%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -0,0 +1,11 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        for i in range(len(s)-1):
+            if abs(ord(s[i])-ord(s[i+1]))==32:
+                return self.makeGood(s[:i]+s[i+2:])
+        return s
+
+
+
+
+        

1d4e0cd58ea119b172dae011328df5d67bab7baf
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-04-06 19:58:32 +0300

    Time: 137 ms (12.36%) | Memory: 18 MB (89.70%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,50 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        # traverse from both sides and get the invalid parentheses in set
+        omitforward = set()
+        stackforward = []
+        for i in range(len(s)):
+
+            cur = s[i]
+            
+
+            if cur== "(" or cur==")":
+                if cur == "(":
+                    stackforward.append("(")
+                elif not stackforward and cur==")":
+                    omitforward.add(i)
+                elif stackforward and cur == ")":
+                    if stackforward[-1] != "(":
+                        omitforward.add(i)
+                    elif stackforward[-1] == "(":
+                        stackforward.pop()
+        omitbackward = set()
+        stackbackward = []
+        for i in range(len(s)-1,-1,-1):
+            cur = s[i]
+            if cur == ")" or cur == "(":
+                if cur == ")":
+                    stackbackward.append(")")
+                elif not stackbackward and cur=="(":
+                    omitbackward.add(i)
+                elif stackbackward and cur == "(":
+                    if stackbackward[-1] != ")":
+                        omitbackward.add(i)
+                    elif stackbackward[-1] == ")":
+                        stackbackward.pop()
+        print(omitforward)
+        print(stackforward)
+        ans = ""
+        for i,letter in enumerate(s):
+            if i not in omitforward and i not in omitbackward:
+                ans += letter
+        
+
+
+        print(stackbackward)
+        print(omitbackward)
+        print(omitbackward)
+        return ans
+        return "lee(t(c)o)de"
+
+        

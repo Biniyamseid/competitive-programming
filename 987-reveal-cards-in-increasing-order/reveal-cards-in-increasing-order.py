6039b0e28c12d7d08b2bbaa1b22c2fa645756191
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-04-10 21:17:55 +0300

    Time: 55 ms (8.61%) | Memory: 16.9 MB (36.98%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -0,0 +1,33 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        N = len(deck)
+        result = [0]*N
+        skip = False
+        index_in_deck = 0
+        index_in_result = 0
+        deck.sort()
+
+        while index_in_deck < N:
+            if result[index_in_result] == 0:
+                if not skip:
+                    result[index_in_result] = deck[index_in_deck]
+                    index_in_deck += 1
+                skip = not skip
+            index_in_result = (index_in_result + 1)%N
+
+        return result
+        # ans = sorted(deck)
+        # answer = []
+        # i = 0
+        # j = len(deck)-1
+        # while i<=j:
+        #     if i!=j:
+        #         answer.append(ans[i])
+        #         answer.append(ans[j])
+        #     else:
+        #         answer.append(ans[i])
+        #     i+=1
+        #     j-=1
+        # return answer
+
+        

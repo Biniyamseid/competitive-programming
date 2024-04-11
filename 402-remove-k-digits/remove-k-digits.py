6afa4e810f3bd19f50f14df00ff1a02ff084b6b4
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-04-11 15:03:51 +0300

    Time: 3790 ms (5.19%) | Memory: 20.9 MB (5.26%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.py
+++ 402-remove-k-digits/remove-k-digits.py
@@ -16,25 +16,6 @@ the best way is to remove the first or the second value
 
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        # num = [int(p) for p in num]
-        # while k>=1:
-            
-        #     # if those are equal go and take the last one
-        #     if num[0] == num[1]:
-        #         j = 0
-        #         while j<len(num)-1 and num[j]==num[j+1]:
-        #             j+=1
-        #         print(j)
-        #         if num[j]>=num[j+1]:
-        #             print(num[0:j],num[j+1:])
-        #             num = num[0:j] + num[j+1:]
-        #         else:
-        #             print(num[0:j],num[j+1:])
-        #             num = num[0:j+1] + num[j+2:]
-        #     k-=1
-        # num = [str(i) for i in num]
-        # return "".join(num)
-                
         if k >= len(num):
             return "0"
         """
@@ -54,10 +35,6 @@ class Solution:
                     stack.pop()
                     k-=1
                 stack.append(c)
-                # k-=1
-            # elif c>stack[-1] and k<=1:
-            #     k-=1
-            # else:
             else:
                 stack.append(c)
                
@@ -72,40 +49,6 @@ class Solution:
         
         return "".join(stack)
             
-
-        # take a stack if there comes minimum value pop the greate and insert the smaller
-
-        # removed_indices = set()
-        
-        # i = 0
-        # num = [int(i) for i in num]
-        # print(num)
-        # while k>=1:
-        #     if len(num)>1:
-        #         if num[0] == num[1]:
-        #             j = 0
-        #             while j<len(num)-2 and num[j]==num[j+1]:
-        #                 j+=1
-        #             print(j)
-        #             if num[j]>=num[j+1]:
-        #                 print(num[0:j],num[j+1:])
-        #                 num = num[0:j] + num[j+1:]
-        #             else:
-        #                 print(num[0:j],num[j+1:])
-        #                 num = num[0:j+1] + num[j+2:]
-
-        #         elif num[0]>num[1]:
-        #             removed_indices.add(0)
-        #             num = num[1:]
-        #         elif num[0]<=num[1]:
-        #             removed_indices.add(1)
-        #             num = [num[0]]+num[2:]
-        #     k-=1
-        # print(num)
-        # while num[0]==0 and len(num)>1:
-        #     num = num[1:]
-        # num = [str(i) for i in num]
-        # return "".join(num)
 
             
             

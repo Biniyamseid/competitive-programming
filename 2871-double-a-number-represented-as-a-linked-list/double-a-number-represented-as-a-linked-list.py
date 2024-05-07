6ad09555568a43f1a242a5e226a7e7095560b648
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-05-07 14:49:16 -0400

    Time: 371 ms (21.59%) | Memory: 19.1 MB (95.46%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.py
@@ -13,29 +13,9 @@ class Solution:
             num += str(cur.val)
             cur = cur.next
         num = str(int(num)*2)
-        print(num)
-        i = 0
         d = a = dummy = head
-        print("hello")
-        ans = ListNode()
-
-        # while dummy:
-        #     ans.next = ListNode(int(num[i]))
-        #     ans = ans.next
-        #     dummy.val = int(num[i])
-        #     print(dummy)
-        #     i+=1
-        #     dummy = dummy.next
-        # i = 0
         for i in range(len(num)):
             c = ListNode(int(num[i]))
             d.next = c
             d = d.next
-        # return head
-
-
-        print(d)
-        print("hello")
         return a.next
-        return head
-        # 

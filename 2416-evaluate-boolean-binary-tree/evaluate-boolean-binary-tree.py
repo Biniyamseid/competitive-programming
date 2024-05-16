0a38e61e821a1c4b4a626e0844830bab2bc00ef9
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-05-15 21:17:38 -0400

    Time: 58 ms (5.64%) | Memory: 16.9 MB (11.53%) - LeetSync

---
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            # if not node.left and not node.right:
+            #     return root.val !=0
+            # evaluate_left_subtree = dfs(node.left)
+            # evaluate_right_subtree = dfs(node.right)
+            # if root.val ==2:
+            #     evaluate_root = evaluate_left_subtree or evaluate_right_subtree
+            # else:
+            #     evaluate_root = evaluate_left_subtree and evaluate_right_subtree
+            # return evaluate_root
+            if not root.left and not root.right:
+                # Handles the case for leaf nodes.
+                return root.val != 0
+
+            # Store the evaluations for the left subtree and right subtree.
+            evaluate_left_subtree = self.evaluateTree(root.left)
+            evaluate_right_subtree = self.evaluateTree(root.right)
+            if root.val == 2:
+                evaluate_root = evaluate_left_subtree or evaluate_right_subtree
+            else:
+                evaluate_root = evaluate_left_subtree and evaluate_right_subtree
+
+            return evaluate_root
+            
+        return dfs(root)
+       
+        

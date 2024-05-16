0cabff1f442ef7c1bc3b8bcd43ea57e8f29378a8
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-05-15 21:18:08 -0400

    Time: 48 ms (51.17%) | Memory: 16.8 MB (92.02%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.py
@@ -21,8 +21,8 @@ class Solution:
                 return root.val != 0
 
             # Store the evaluations for the left subtree and right subtree.
-            evaluate_left_subtree = self.evaluateTree(root.left)
-            evaluate_right_subtree = self.evaluateTree(root.right)
+            evaluate_left_subtree = dfs(root.left)
+            evaluate_right_subtree = dfs(root.right)
             if root.val == 2:
                 evaluate_root = evaluate_left_subtree or evaluate_right_subtree
             else:

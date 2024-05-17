f8776599066014d2354c45c9671059b68f4ba28f
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-05-17 15:54:22 -0400

    Time: 39 ms (91.12%) | Memory: 17.3 MB (29.11%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -7,12 +7,15 @@
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
         if not root:
-            return None
+            return
         root.left = self.removeLeafNodes(root.left,target)
         root.right = self.removeLeafNodes(root.right,target)
-        if root.left is None and root.right is None and root.val == target:
+        if not root.left and not root.right and root.val == target:
             return None
         return root
+
+        
+
 
         
         

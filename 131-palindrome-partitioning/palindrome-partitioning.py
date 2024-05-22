54f1961002b5a78e68c265409ed3de351a0603bf
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-05-22 22:47:09 +0300

    Time: 498 ms (17.30%) | Memory: 35.3 MB (41.65%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -5,16 +5,11 @@ class Solution:
         def ispalindrome(string):
             return string == string[::-1]
         def dfs(str,i,visited):
-            print(str,i,visited)
             if i == len(s):
-                print(visited)
-                already.append(visited)
                 ans.append(visited[:])
                 return
             for j in range(i,len(s)+1):
-                print(str[i:j+1],"second loop")
                 if ispalindrome(str[i:j+1]):
-                    print(str[i:j])
                     dfs(str,j+1,visited+[str[i:j+1]])
         ans = []
         dfs(s,0,[])

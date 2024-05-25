2390bfc3a2653dde30761b82288ba133edd14477
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-05-25 07:31:45 +0300

    Time: 35 ms (53.72%) | Memory: 16.4 MB (99.01%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -2,13 +2,10 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         ans = []
         def dfs(i,word,curlist):
-            print(i,word,curlist)
-
             if i == len(s):
                 ans.append(curlist)
                 return 
             for j in range(i,len(s)+1):
-                
                 if s[i:j+1] in wordDict:
                     if curlist:
                         dfs(j+1,s[i:j+1],curlist+" " +s[i:j+1])

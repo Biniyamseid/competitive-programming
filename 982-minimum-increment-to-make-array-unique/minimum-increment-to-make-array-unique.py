9859a472a963b4992496f1a0c78353ed46f92396
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-06-13 21:13:55 -0400

    Time: 916 ms (5.18%) | Memory: 47.7 MB (5.86%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -0,0 +1,45 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        """
+        nums = [3,2,1,2,1,7]
+        i = 0
+        nextint = [4,5,6]
+        1,1,2,2,3,7
+        1-4 = 3
+        i+=1
+        2-5 = 3
+
+        """
+        nums.sort()
+        minm = min(nums)
+        nextint = []
+        set1 = set(nums)
+        for i in range(minm+1,len(nums)+max(nums)+1):
+            if i not in set1:
+                nextint.append(i)
+        print(nextint)
+        for i in range(max(nextint)+1,max(nextint)+1+len(nums)):
+            nextint.append(i)
+        ans = 0
+        i = 0
+        visited = set()
+        count = Counter(nums)
+        for num in nums:
+            if count[num]>1 and num in visited:
+                while nextint[i]<=num:
+                    i+=1
+            if count[num]>1 and num in visited:
+                print(num,i,nextint[i])
+                ans += abs(nextint[i]-num)
+                i+=1
+            visited.add(num)
+        return ans
+
+
+        
+        # print([i for i in range(minm+1,max(len(nums),max(nums)))])
+        # for i,j in zip(nums,[i for i in range(minm+1,max(len(nums),max(nums)))]):
+        #     print(i,j)
+        
+
+        

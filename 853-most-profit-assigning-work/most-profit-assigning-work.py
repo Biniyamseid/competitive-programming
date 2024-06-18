deebe9f808205232c99cac41bbf59d1840081738
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-06-17 22:47:24 -0400

    Time: 274 ms (60.90%) | Memory: 19.9 MB (11.96%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.py
+++ 853-most-profit-assigning-work/most-profit-assigning-work.py
@@ -1,40 +1,34 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        """
-        Input: difficulty = [2,4,6,8,10], profit = [10,20,30,40,50], worker = [4,5,6,7]
-        worker.sort(reverse=True)
-        [7,6,5,4]
-        l = len(diff)-1
-        for i in worker:
-            if diff[l]>i:
-                l-=1
-            else:
-                ans+=diff[l]
-            if l<0:
-                break
-        return ans
-        """
+
+        """pre processing"""
         list_zip = list(zip(difficulty,profit))
         list_zip.sort()
-       
         difficulty,profit = zip(*list_zip)
-        
+        worker.sort(reverse=True)
         profit = list(profit)
+
+        """assume"""
+
+        # assume as difficulty increases profit also increases, if you can afford the maximum difficulty you can surely afford the minimum difficulty,  so what stops you from taking the profit from the minimum difficulty while you can do a more difficult work, (let's assume again you are lazy :),so always choose an affordable maximum profit)
         max_so_far = 0
         for i,j in enumerate(difficulty):
             if max_so_far > profit[i]:
                 profit[i] = max_so_far
             max_so_far = max(max_so_far,profit[i])
 
+
+        # final
+
         ans = 0
-        worker.sort(reverse=True)
+
         l = len(difficulty)-1
         for i in worker:
             while difficulty[l]>i and l>=0:
                 l-=1
             if l<0:
                 break
-            
             ans+=profit[l]
+
         return ans
         

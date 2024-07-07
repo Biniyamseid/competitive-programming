422075430ceb7be1e08518b372449020d024628a
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2024-07-07 21:03:47 +0200

    Time: 22 ms (9.09%) | Memory: 11.6 MB (24.24%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -7,14 +7,13 @@ class Solution(object):
         """
         a = numBottles
         recurr = (numBottles)*2
-        remain = 0
+
         ans = numBottles
-        while numBottles< a*2 and recurr>0:
+        while recurr>0:
             temp =  numBottles % numExchange
             ans += numBottles//numExchange
             
             numBottles =temp +numBottles//numExchange
-            print(numBottles)
 
             recurr -=1
         

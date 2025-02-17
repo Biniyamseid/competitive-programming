0e6918df24dd8f2d5317a6aefc8c44fd2125afef
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-02-17 08:59:05 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (100.00%) - LeetSync

--- 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.py
+++ 3709-find-special-substring-of-length-k/find-special-substring-of-length-k.py
@@ -1,43 +1,26 @@
 class Solution:
     def hasSpecialSubstring(self, s: str, k: int) -> bool:
-        l = 0
-        traverse = False
         left = 0
- 
+        def check(left,right):
+             if len(s[left:right+1]) == k:
+                    if i<len(s)-1:
+                        if s[right]!=s[right+1]:
+                            return True
+                    if right==len(s)-1:
+                            return True
+             return False
+
+
         for i in range(0,len(s)):
-            print(i,left)
+           
             if i>0 and  s[i] == s[i-1]:
-                l+=1
-                if len(s[left:i+1]) == k:
-                    if i<len(s)-1:
-                        if s[i]!=s[i+1]:
-                            return True
-                    if i==len(s)-1:
-                            return True
+                if check(left,i):return True
+             
             elif i==0:
-                if len(s[left:i+1]) == k:
-                    if i<len(s)-1:
-                        if s[i]!=s[i+1]:
-                            return True
-                    if i==len(s)-1:
-                            return True
-
-
-                # if l ==k-1:
-                #     if i<len(s)-1:
-                #         if s[i]!=s[i+1]:
-                #             return True
-                #     if i==len(s)-1:
-                #         return True
+                if check(left,i):return True
             else:
                 left=i
-                l = 0
-                if len(s[left:i+1]) == k:
-                    if i<len(s)-1:
-                        if s[i]!=s[i+1]:
-                            return True
-                    if i==len(s)-1:
-                            return True
+                if check(left,i):return True
         return False
 
 

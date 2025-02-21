a466a6e02a96aa1f5c3e666966f4fa843c93f51d
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-02-22 01:16:56 +0300

    Time: 61 ms (34.94%) | Memory: 33.3 MB (74.81%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,30 +1,57 @@
-from typing import List
-
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        num_map = {}  # To store left/right boundary information
+        my_set = set(nums)
         visited = set()
-        max_length = 0
-
+        ans = 0
         for num in nums:
-            if num in visited:  # Skip duplicates
+            if num in visited:
                 continue
+            # is num starting point
+            if num-1 in my_set:
+                continue
+            cur_len = 1
             visited.add(num)
-
-            left = num_map.get(num - 1, 0)  # Get left sequence length
-            right = num_map.get(num + 1, 0)  # Get right sequence length
-            total_length = left + right + 1  # Total sequence length
-
-            num_map[num] = total_length  # Store at the current num
-            num_map[num - left] = total_length  # Update left boundary
-            num_map[num + right] = total_length  # Update right boundary
+            while num+1 in my_set:
+                cur_len+=1
+                num+=1
+            ans = max(ans,cur_len)
             
-            max_length = max(max_length, total_length)  # Update max length
+        return ans
+            
+            
+            
+
+
+            
+
+
+# from typing import List
+
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         if not nums:
+#             return 0
         
-        return max_length
+#         num_map = {}  # To store left/right boundary information
+#         visited = set()
+#         max_length = 0
+
+#         for num in nums:
+#             if num in visited:  # Skip duplicates
+#                 continue
+#             visited.add(num)
+
+#             left = num_map.get(num - 1, 0)  # Get left sequence length
+#             right = num_map.get(num + 1, 0)  # Get right sequence length
+#             total_length = left + right + 1  # Total sequence length
+
+#             num_map[num] = total_length  # Store at the current num
+#             num_map[num - left] = total_length  # Update left boundary
+#             num_map[num + right] = total_length  # Update right boundary
+            
+#             max_length = max(max_length, total_length)  # Update max length
+        
+#         return max_length
 
 
 # class Solution:

8e90d0e59a5f5e1aaed0e869e095088c3a80feec
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-02-22 02:02:54 +0300

    Time: 121 ms (54.87%) | Memory: 31.6 MB (66.06%) - LeetSync

--- 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.py
+++ 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxConsecutive(self, bottom: int, top: int, special: List[int]) -> int:
+        special.append(bottom -1)
+        special.append(top +1)
         special.sort()
-        starting = bottom
-        left = special[0]- starting
-        right = top-special[-1] 
-        ans = max(right,left)
+        ans = 0
         for i in range(1,len(special)):
             ans = max(ans,special[i]-special[i-1]-1)
         return ans

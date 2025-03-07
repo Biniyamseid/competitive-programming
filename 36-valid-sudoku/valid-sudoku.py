63e19af9aa6aeda4129b8e5bdf8aca69a4d14333
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-03-08 00:31:18 +0300

    Time: 19 ms (5.26%) | Memory: 17.9 MB (25.11%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -11,6 +11,8 @@ class Solution:
         for i in range(len(board)):
             for j in range(len(board[0])):
                 curr = board[i][j]
+                if curr == ".":
+                    continue
                 row_val[i].append(curr)
                 col_val[j].append(curr)
                 th_i = int(math.floor(i/3))
@@ -22,24 +24,21 @@ class Solution:
 
 
 
-        print(row_val)
-        print(col_val)
-        print()
-        print(three_by_three)
+        
         for i in row_val:
-            lists = [j for j in i if j!="."]
-            if len(lists)!=len(set(lists)):
-                print(lists)
+           
+            if len(i)!=len(set(i)):
+              
                 return False
         for i in col_val:
-            lists = [j for j in i if j!="."]
-            if len(lists)!=len(set(lists)):
-                print(lists)
+            
+            if len(i)!=len(set(i)):
+                
                 return False
         for i in three_by_three:
-            lists = [j for j in i if j!="."]
-            if len(lists)!=len(set(lists)):
-                print(lists)
+            
+            if len(i)!=len(set(i)):
+               
                 return False
         return True
 

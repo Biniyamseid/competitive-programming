dc51d41cfa1ac41f549baaaff2870b8340f76160
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-04-23 00:22:11 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.53%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,15 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        def generate_subsets(index,nums,subset,subsets):
-            if index == len(nums):
-                subsets.append(subset[:])
-                return
-            subset.append(nums[index])
-            generate_subsets(index+1,nums,subset,subsets)
-            subset.pop()
-            generate_subsets(index+1,nums,subset,subsets)
-            return subsets
-        return generate_subsets(0,nums,[],[])
+        nums.sort()
+        ans = []
+        def backtrack(sol,n):
+            if len(sol)== len(set(sol)):
+                ans.append(sol.copy())
+            for i in range(n,len(nums)):
+                sol.append(nums[i])
+                backtrack(sol,i+1)
+                sol.pop()
 
-            
+        backtrack([],0)
+        return ans
         

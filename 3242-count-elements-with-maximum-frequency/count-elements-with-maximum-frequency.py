320f53d6d32a1c50eb601d5bc5a9ee0170f5e20c
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-09-22 03:15:28 +0300

    Time: 3 ms (34.62%) | Memory: 17.9 MB (15.03%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -1,8 +1,23 @@
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
-        maxm = max(Counter(nums).values())
-        ans = 0
-        for i in Counter(nums):
-            if Counter(nums)[i]==maxm:ans +=Counter(nums)[i]
-        return ans
+        # [1,2,3,4,5]
+        # find maximum frequency
+        # find numbers with maximum frequency
+        #return their count or max frequency*count(frequnet numbers)
+
+        # make a map of number to frequency
+        # find the maximum frequency
+        # find numbers with this maxfrequency 
+        # multiple count with frequency
+
+        frequency_map = {i:0 for i in nums}
+        for num in nums:
+            frequency_map[num]+=1
+        max_frequency = max([j for i,j in frequency_map.items()])
+        max_frequent_number_count = 0
+        for i,j in frequency_map.items():
+            if j == max_frequency:
+                max_frequent_number_count += 1
+        return max_frequency * max_frequent_number_count
+
         

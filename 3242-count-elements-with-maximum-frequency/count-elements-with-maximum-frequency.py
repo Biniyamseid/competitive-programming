42735454b5b5b9354d1d64780d2c89a321eaf062
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-09-22 03:52:14 +0300

    Time: 3 ms (34.62%) | Memory: 17.6 MB (97.86%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -1,5 +1,23 @@
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
+        #
+        total_frequency = 0
+        max_frequency = 0
+        frequency = {num:0 for num in nums }
+        for num in nums:
+            frequency[num]+=1
+            current_frequency = frequency[num]
+            
+            
+            if current_frequency > max_frequency:
+                max_frequency = current_frequency
+                total_frequency = max_frequency
+            elif current_frequency == max_frequency:
+                total_frequency += max_frequency
+        return total_frequency
+            
+
+
         # [1,2,3,4,5]
         # find maximum frequency
         # find numbers with maximum frequency
@@ -10,18 +28,33 @@ class Solution:
         # find numbers with this maxfrequency 
 
 
-        frequency = [0]*100
-        for num in nums:
-            frequency[num-1]+=1
-        frequency.sort()
-        max_freq = frequency[-1]
-        total_sum = 0
-        for freq in frequency[::-1]:
-            if freq!= max_freq:
-                break
-            total_sum += freq
+        # frequency = [0]*100
+        # for num in nums:
+        #     frequency[num-1]+=1
+        # frequency.sort()
+        # max_freq = frequency[-1]
+        # total_sum = 0
+        # for freq in frequency[::-1]:
+        #     if freq!= max_freq:
+        #         break
+        #     total_sum += freq
+
+        # return total_sum
+
+
+        # max_freq = 0
+        # total_freq = 0
+        # for num in nums:
+        #     if num== max_freq:
+        #         total_freq+=num
+        #     if num > max_freq:
+        #         max_freq = num
+        #         total_freq = max_freq
+        # return total_freq
+        
+        
      
-        return total_sum
+        # return total_sum
 
 
         # # multiple count with frequency

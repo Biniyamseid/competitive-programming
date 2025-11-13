e440fc0c13625c05a0238ed112e9b52f3759d6d9
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-11-13 11:58:39 +0300

    Time: 251 ms (5.39%) | Memory: 19.9 MB (8.38%) - LeetSync

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.py
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -1,20 +1,18 @@
 class Solution:
     def maxOperations(self, s: str) -> int:
-        # find how many ones
-        # allocate last how many ones position to ones and if they already occupied no problem but if it is not occupied calculate.
-        r = 0
-        # l = 0 ,if left[l] == "1" find an r for it, replace that place with one and continue for the next 1.
+        # remove consecutive "0" s we don't need them and  divide into left and right, then count
 
+        r = 0
         s_new = s[0]
         for i in range(1,len(s)):
             if s[i]=="0" and s[i-1]=="0":
                 continue
             s_new += s[i]
         s= s_new
+
         count_of_ones = s.count("1")
         left,right = s[0:len(s)-count_of_ones],s[len(s)-count_of_ones:]
-        # if right.count("1") == count_of_ones:
-        #     return 0
+        # convert to array
         left = [i for i in left]
         right = [i for i in right]
 
@@ -31,7 +29,7 @@ class Solution:
                     if right[r]=="0":
                         right[r]="1"
                         # counter += (len(left)-l+r-1)
-                        left_walk = nl-l-1
+                        left_walk = len(left)-l-1
                         right_walk = r+1
                         counter+=left_walk+right_walk
                         # print(right[r])
@@ -39,5 +37,15 @@ class Solution:
                     r+=1
         print(left,right)
         return counter
+
+
+
+
+
+
+        # find how many ones
+        # allocate last how many ones position to ones and if they already occupied no problem but if it is not occupied calculate.
+        
+        # l = 0 ,if left[l] == "1" find an r for it, replace that place with one and continue for the next 1.
 
         

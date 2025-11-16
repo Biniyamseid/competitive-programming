3d1963a8dc4fab9445f582fa9877b770fdc9bf5d
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-11-17 00:55:25 +0300

    Time: 40 ms (5.63%) | Memory: 18.5 MB (15.14%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.py
@@ -2,6 +2,7 @@ class Solution:
     def numSub(self, s: str) -> int:
         #  s = "0110111"
         #  get consecutive ones and append n! for answer
+        # 1
         res = 0
         i = 0
         while (i<len(s)):
@@ -13,6 +14,15 @@ class Solution:
                     res += ((temp_count) * (temp_count+1))//2
                 else:
                     i+=1
+        # return res%(10**9+7)
+
+        res = 0
+
+        # 2
+        splits = s.split("0")
+        for split in splits:
+            res += ((len(split))*(len(split)+1))//2
         return res%(10**9+7)
+            
             
         

ed315e4b0ac49a6c01abc6d93cb2ff47c798d438
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2025-12-04 02:58:48 +0300

    Time: 877 ms (99.67%) | Memory: 68.2 MB (98.83%) - LeetSync

---
+++ 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.py
@@ -0,0 +1,53 @@
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        n = len(points)
+        inf = 10**9 + 7
+        slope_to_intercept = defaultdict(list)
+        mid_to_slope = defaultdict(list)
+        ans = 0
+
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(i + 1, n):
+                x2, y2 = points[j]
+                dx = x1 - x2
+                dy = y1 - y2
+
+                if x2 == x1:
+                    k = inf
+                    b = x1
+                else:
+                    k = (y2 - y1) / (x2 - x1)
+                    b = (y1 * dx - x1 * dy) / dx
+
+                mid = (x1 + x2) * 10000 + (y1 + y2)
+                slope_to_intercept[k].append(b)
+                mid_to_slope[mid].append(k)
+
+        for sti in slope_to_intercept.values():
+            if len(sti) == 1:
+                continue
+
+            cnt = defaultdict(int)
+            for b_val in sti:
+                cnt[b_val] += 1
+
+            total_sum = 0
+            for count in cnt.values():
+                ans += total_sum * count
+                total_sum += count
+
+        for mts in mid_to_slope.values():
+            if len(mts) == 1:
+                continue
+
+            cnt = defaultdict(int)
+            for k_val in mts:
+                cnt[k_val] += 1
+
+            total_sum = 0
+            for count in cnt.values():
+                ans -= total_sum * count
+                total_sum += count
+
+        return ans

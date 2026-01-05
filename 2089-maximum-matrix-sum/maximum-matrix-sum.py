1143e5d0f2edb377fbbfbe542b52092f1febf664
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2026-01-06 02:26:16 +0300

    Time: 102 ms (36.31%) | Memory: 26.6 MB (19.75%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.py
@@ -1,24 +1,17 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        alllist = []
+        minm = float(inf)
         total = 0
-        negatives = []
         count = 0
         for i in  range(len(matrix)):
          for j in range(len(matrix[0])):
-            alllist.append(abs(matrix[i][j]))
-            if matrix[i][j]<=0:
-                negatives.append(matrix[i][j])
-            else:
-                total+=matrix[i][j]
-        if len(negatives)%2==0:
-            if negatives:
-             total += abs(sum(negatives))
-        else:
-            if negatives:
-                negatives = sorted(negatives,reverse = True)
-                total += abs(sum(negatives[1:]))+abs(negatives[0])
-                total-= 2*min(alllist)
+            curr = matrix[i][j]
+            minm = min(minm,abs(curr))
+            if curr<=0:
+                count +=1
+            total+=abs(curr)
+        if count%2!=0:
+                total-= 2*minm
         return total
             
 

27012b38cc78da5a57809ae0711fd172de1a959f
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2026-01-16 02:15:28 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (17.81%) - LeetSync

--- 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.py
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.py
@@ -2,44 +2,28 @@ class Solution:
     def maximizeSquareHoleArea(self, n: int, m: int, hBars: List[int], vBars: List[int]) -> int:
         hBars.sort()
         vBars.sort()
-        maxvconsec = 0
-        maxhconsec = 0
-        curr = 0
-        conseq = 0
+        maxvconsec = 1
+        maxhconsec = 1
+        curr = 1
         for i in range(1,len(hBars)):
             if hBars[i]==hBars[i-1]+1:
-                if conseq:curr+=1
-                else: 
-                    curr =2
-                    conseq = 1
-                maxhconsec= max(maxhconsec,curr)
+                curr+=1
             else:
-                maxhconsec= max(maxhconsec,curr)
                 curr=1
-                conseq = 0
-        curr = 0
-        conseq=0
+            maxhconsec= max(maxhconsec,curr)
+        curr = 1
         for i in range(1,len(vBars)):
             if vBars[i]==vBars[i-1]+1:
-                if conseq:
-                    curr+=1
-                else: 
-                    curr =2
-                    conseq = 1
-                maxvconsec= max(maxvconsec,curr)
+                curr+=1
             else:
                 curr=1
-                conseq = 0
-                maxvconsec= max(maxvconsec,curr)
+            maxvconsec= max(maxvconsec,curr)
        
        
 
-        if maxhconsec<1: 
-            maxhconsec=1
-        if maxvconsec<1: 
-            maxvconsec=1
-        ans = min(maxhconsec,maxvconsec)
-        return (ans+1)*(ans+1)
+        
+        ans = min(maxhconsec,maxvconsec)+1
+        return (ans)*(ans)
         
 
         

a3342cac80dad8806e35a5d42ed0c64aa55c08f7
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2026-01-21 00:53:12 +0300

    Time: 139 ms (38.41%) | Memory: 19.5 MB (5.80%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.py
@@ -1,15 +1,17 @@
 class Solution:
     def minBitwiseArray(self, nums: List[int]) -> List[int]:
-        bitormap = defaultdict(int)
-        for i in range(1001):
-            result = i | i+1
-            if bitormap[result]==-0:
-                bitormap[result] = i
-        answer = []
-        for i in nums:
-                if bitormap[i] == 0:
-                    answer.append(-1)
-                else:
-                    answer.append(bitormap[i])
-        return answer
+       ans = []
+       for num in nums:
+        answer = False
+        for candidate in range(1,num):
+            if candidate | (candidate+1)  == num:
+                ans.append(candidate)
+                answer = True
+                break
+        if not answer:
+            ans.append(-1)
+       return ans
+        
+        
+
             

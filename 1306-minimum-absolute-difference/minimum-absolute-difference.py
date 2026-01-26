042b2b60dff1c66fbb99361d7f79dd4f2ef6a08e
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2026-01-26 23:37:56 +0300

    Time: 43 ms (94.04%) | Memory: 31.5 MB (23.34%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.py
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.py
@@ -4,9 +4,13 @@ class Solution:
         minm = inf
         answer = []
         for i in range(len(arr)-1):
-            minm = min(arr[i+1]-arr[i],minm)
-        for i in range(len(arr)-1):
-            if arr[i+1]-arr[i] == minm:
+            diff  = arr[i+1]-arr[i]
+            if diff<minm:
+                while answer:
+                    answer.pop()
+                minm = diff
+                answer.append([arr[i],arr[i+1]])
+            elif diff == minm:
                 answer.append([arr[i],arr[i+1]])
         return answer
 

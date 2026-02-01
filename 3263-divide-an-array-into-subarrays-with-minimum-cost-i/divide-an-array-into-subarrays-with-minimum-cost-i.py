a25dfbb76ee3d67cbbcf77dcbf4f436710d2cae1
Biniyam Seid <88603119+Biniyamseid@users.noreply.github.com>
2026-02-02 01:31:36 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (50.26%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -5,8 +5,7 @@ class Solution:
         firstmin = float(inf)
         for num in nums[1:]:
             if num< firstmin:
-                if firstmin<=secondmin:
-                    secondmin= firstmin
+                secondmin= firstmin
                 firstmin=num
             elif num<=secondmin:
                 secondmin=num
